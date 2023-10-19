13691c589b358be4ba9f42479b98fba5ef73fb2a
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-10-19 11:40:16 -0400

    Delete download-and-build.sh

--- download-and-build.sh
+++
@@ -1,7 +0,0 @@
-printf -v date '%(%Y-%m-%d)T\n' -1
-cp pgl-dns.service /etc/systemd/system/pgl-dns.service
-git clone https://github.com/NDSLink/dream-server
-git clone https://github.com/NDSLink/DNS-Server
-cd dream-server
-docker build -t dream-server:$date -t dream-server:latest .
-docker run docker run -d -p 80:80 --name dream-server dream-server:latest
