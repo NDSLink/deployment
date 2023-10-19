4f13fdd167265eb170ff916d9521617c5d92d19e
EC2 Default User <ec2-user@ip-172-31-14-97.us-east-2.compute.internal>
2023-10-19 13:46:15 +0000

    Add requirements installer

---
+++ install-reqs.sh
@@ -0,0 +1,9 @@
+sudo yum install python3.11
+sudo yum install python3-pip
+sudo yum install docker
+sudo yum install git
+sudo systemctl start docker
+sudo usermod -a -G docker $USER
+sudo yum install npm
+sudo npm install -g node-sass
+sudo pip3 install -r DNS-Server/requirements.txt
